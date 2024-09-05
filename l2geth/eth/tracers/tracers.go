7de6174ff994f082ba1d28fc4af52172f60ea1fe
moodbased <moodbased@outlook.com>
2024-09-05 16:54:03 +0800

    rm unused GetMemoryCopyPadded

--- l2geth/eth/tracers/tracers.go
+++ l2geth/eth/tracers/tracers.go
@@ -19,8 +19,6 @@ package tracers
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"strings"
 	"unicode"
 
@@ -61,28 +59,4 @@ func tracer(name string) (string, bool) {
 		return tracer, true
 	}
 	return "", false
-}
-
-const (
-	memoryPadLimit = 1024 * 1024
-)
-
-// GetMemoryCopyPadded returns offset + size as a new slice.
-// It zero-pads the slice if it extends beyond memory bounds.
-func GetMemoryCopyPadded(m *vm.Memory, offset, size int64) ([]byte, error) {
-	if offset < 0 || size < 0 {
-		return nil, errors.New("offset or size must not be negative")
-	}
-	if int(offset+size) < m.Len() { // slice fully inside memory
-		return m.GetCopy(offset, size), nil
-	}
-	paddingNeeded := int(offset+size) - m.Len()
-	if paddingNeeded > memoryPadLimit {
-		return nil, fmt.Errorf("reached limit for padding memory slice: %d", paddingNeeded)
-	}
-	cpy := make([]byte, size)
-	if overlap := int64(m.Len()) - offset; overlap > 0 {
-		copy(cpy, m.GetPtr(offset, overlap))
-	}
-	return cpy, nil
 }
